5ac1f71b54a3e11b1443d0cdfe011094fd40472d
satomic <yxfdental@126.com>
2025-12-20 13:14:35 +0800

    Fix URL formatting in Elasticsearch index creation script

--- src/cpuad-updater/create_es_indexes.sh
+++ src/cpuad-updater/create_es_indexes.sh
@@ -1,7 +1,7 @@
-curl -X PUT "http://$ELASTICSEARCH_URL/copilot_usage_total" -H 'Content-Type: application/json' -d @mapping/copilot_usage_total_mapping.json
-curl -X PUT "http://$ELASTICSEARCH_URL/copilot_usage_breakdown" -H 'Content-Type: application/json' -d @mapping/copilot_usage_breakdown_mapping.json
-curl -X PUT "http://$ELASTICSEARCH_URL/copilot_usage_breakdown_chat" -H 'Content-Type: application/json' -d @mapping/copilot_usage_breakdown_chat_mapping.json
-curl -X PUT "http://$ELASTICSEARCH_URL/copilot_seat_info_settings" -H 'Content-Type: application/json' -d @mapping/copilot_seat_info_settings_mapping.json
-curl -X PUT "http://$ELASTICSEARCH_URL/copilot_seat_assignments" -H 'Content-Type: application/json' -d @mapping/copilot_seat_assignments_mapping.json
-curl -X PUT "http://$ELASTICSEARCH_URL/copilot_user_adoption" -H 'Content-Type: application/json' -d @mapping/copilot_user_adoption_mapping.json
-curl -X PUT "http://$ELASTICSEARCH_URL/copilot_user_metrics" -H 'Content-Type: application/json' -d @mapping/copilot_user_metrics_mapping.json
+curl -X PUT "$ELASTICSEARCH_URL/copilot_usage_total" -H 'Content-Type: application/json' -d @mapping/copilot_usage_total_mapping.json
+curl -X PUT "$ELASTICSEARCH_URL/copilot_usage_breakdown" -H 'Content-Type: application/json' -d @mapping/copilot_usage_breakdown_mapping.json
+curl -X PUT "$ELASTICSEARCH_URL/copilot_usage_breakdown_chat" -H 'Content-Type: application/json' -d @mapping/copilot_usage_breakdown_chat_mapping.json
+curl -X PUT "$ELASTICSEARCH_URL/copilot_seat_info_settings" -H 'Content-Type: application/json' -d @mapping/copilot_seat_info_settings_mapping.json
+curl -X PUT "$ELASTICSEARCH_URL/copilot_seat_assignments" -H 'Content-Type: application/json' -d @mapping/copilot_seat_assignments_mapping.json
+curl -X PUT "$ELASTICSEARCH_URL/copilot_user_adoption" -H 'Content-Type: application/json' -d @mapping/copilot_user_adoption_mapping.json
+curl -X PUT "$ELASTICSEARCH_URL/copilot_user_metrics" -H 'Content-Type: application/json' -d @mapping/copilot_user_metrics_mapping.json
