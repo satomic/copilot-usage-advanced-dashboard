e79a2b802011a15e26668ed5a5c1eb86702f5a00
satomic <yxfdental@126.com>
2025-12-20 13:12:20 +0800

    Add new Elasticsearch index for user adoption and fix formatting in seat assignments mapping

--- src/cpuad-updater/create_es_indexes.sh
+++ src/cpuad-updater/create_es_indexes.sh
@@ -3,3 +3,5 @@ curl -X PUT "http://$ELASTICSEARCH_URL/copilot_usage_breakdown" -H 'Content-Type
 curl -X PUT "http://$ELASTICSEARCH_URL/copilot_usage_breakdown_chat" -H 'Content-Type: application/json' -d @mapping/copilot_usage_breakdown_chat_mapping.json
 curl -X PUT "http://$ELASTICSEARCH_URL/copilot_seat_info_settings" -H 'Content-Type: application/json' -d @mapping/copilot_seat_info_settings_mapping.json
 curl -X PUT "http://$ELASTICSEARCH_URL/copilot_seat_assignments" -H 'Content-Type: application/json' -d @mapping/copilot_seat_assignments_mapping.json
+curl -X PUT "http://$ELASTICSEARCH_URL/copilot_user_adoption" -H 'Content-Type: application/json' -d @mapping/copilot_user_adoption_mapping.json
+curl -X PUT "http://$ELASTICSEARCH_URL/copilot_user_metrics" -H 'Content-Type: application/json' -d @mapping/copilot_user_metrics_mapping.json
